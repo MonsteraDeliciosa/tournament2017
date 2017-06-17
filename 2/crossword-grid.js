dca7363a3f3904a56b8ba3bfbdd8efb3f14010a7
MonsteraDeliciosa <kaja.otrebska@gmail.com>
2017-06-17 23:58:11 +0200

    word matching refactor

--- 2/crossword-grid.js
+++ 2/crossword-grid.js
@@ -1,6 +1,8 @@
+var placedOnBoard = [];
+
 (function drawGrid() {
   // var size = prompt('Enter grid size') || 20;
-  var size = 30;
+  var size = 25;
   var table = document.getElementsByClassName('crossword-table')[0];
 
   for (var y = 0; y < size; y++) {
@@ -41,6 +43,8 @@
   };
 
   xmlhttp.open("GET", "https://rocky-shore-45098.herokuapp.com", true);
+  // xmlhttp.open("GET", "mock-response.json", true);
+
   xmlhttp.send();
 
 })();
@@ -80,7 +84,7 @@ function sortAnswers(response) {
   for (var i = 0; i < response.length; i++) {
     var answer = {
       no: i,
-      text: response[i].answer,
+      text: response[i].answer.toLowerCase(),
       length: response[i].answer.length
     };
 
@@ -90,10 +94,6 @@ function sortAnswers(response) {
   var sortedDesc = answerArr.sort(function(a, b) {
     return b.text.length - a.text.length;
   });
-
-  for (var i = 0; i < response.length; i++) {
-    console.log(sortedDesc[i].text);
-  }
 
   placeWords(sortedDesc);
 }
@@ -110,6 +110,13 @@ function setDirection() {
 
 function createLetterInput(tile, firstWord, letter) {
 
+  if (tile.classList.value.indexOf('empty') >= 0) {
+    tile.classList.remove('empty');
+  } else {
+    tile.classList.add('blocked');
+    // return;
+  }
+
   var input = document.createElement("input");
 
   input.className = "letter-box";
@@ -117,126 +124,96 @@ function createLetterInput(tile, firstWord, letter) {
   input.setAttribute('data-number', firstWord.no);
   input.setAttribute('maxLength', '1');
   input.setAttribute('value', letter);
+
+  //uncomment when debugged
   // input.addEventListener('click', toggleHighlight(input));
 
   tile.appendChild(input);
-  tile.classList.remove('empty');
 }
 
-function placeWords(sortedAnswers, direction, xy, dist) {
-
-  if (!sortedAnswers.length) {
-    return;
-  }
-
-  var firstWord = sortedAnswers[0];
-  var rowsAmount = document.getElementsByClassName('answer-row').length;
-  var dir = direction || setDirection();
-  var randXY = xy || Math.floor(Math.random() * rowsAmount);
-  var maxDist = dist || Math.floor(Math.random() * (rowsAmount - firstWord.length));
-
-
-  if (dir === 'horizontal') {
-    for (var x = 0; x < firstWord.length; x++) {
-      var tile = document.querySelectorAll('[data-x="' + (maxDist + x) + '"][data-y="' + randXY + '"]')[0];
-
-      var letter = firstWord.text[x];
-
-      createLetterInput(tile, firstWord, letter);
-    }
-    searchLetterMatches(sortedAnswers, maxDist, randXY, 'vertical');
-    // placeWords(sortedAnswers, 'vertical');
-  }
-
-  if (dir === 'vertical') {
-    for (var y = 0; y < firstWord.length; y++) {
-      var tile = document.querySelectorAll('[data-y="' + (maxDist + y) + '"][data-x="' + randXY + '"]')[0];
-      var input = document.createElement("input");
-      var letter = firstWord.text[y];
-
-      createLetterInput(tile, firstWord, letter);
-    }
-
-    searchLetterMatches(sortedAnswers, maxDist, randXY, 'horizontal');
-
-    // placeWords(sortedAnswers, 'horizontal');
-  }
-}
-
-function searchLetterMatches(sortedAnswers, maxDist, randXY, direction) {
+function getDistance(sortedAnswers, direction) {
   var word1 = sortedAnswers[0].text;
   var word2 = sortedAnswers[1].text;
 
   for (var i = 0; i < word2.length; i++) {
     var indexOf = word1.indexOf(word2[i]);
+    var matchingWordLen = word2.length;
 
-    if (indexOf > 0) {
-      console.log(word2[i]);
-      console.log(word1[indexOf]);
+    if (indexOf >= 0) {
+      return i;
+    }
+  }
+}
 
-      console.log(word1 + '  ' + word2 + ' ' + indexOf);
+function placeWords(sortedAnswers, direction, randXY, dist) {
+  if (!sortedAnswers.length) {
+    return;
+  }
+
+  var firstWord = sortedAnswers[0];
+  var nextWord = sortedAnswers[1];
+  var rowsColsAmount = document.getElementsByClassName('answer-row').length;
+  var dir = direction || setDirection();
+  var randXY = randXY || getDistance(sortedAnswers);
+  var maxDist = dist || 0;
+
+  if (dir === 'horizontal') {
+
+    for (var x = 0; x < firstWord.length; x++) {
+      var tile = document.querySelectorAll('[data-x="' + (maxDist + x) + '"][data-y="' + randXY + '"]')[0];
+      var letter = firstWord.text[x];
+
+      createLetterInput(tile, firstWord, letter);
+    }
+    searchLetterMatches(sortedAnswers, maxDist, randXY, 'vertical');
+  }
+
+  if (dir === 'vertical') {
+    for (var y = 0; y < firstWord.length; y++) {
+      var tile = document.querySelectorAll('[data-y="' + (maxDist + y) + '"][data-x="' + randXY + '"]')[0];
+      var letter = firstWord.text[y];
+
+      createLetterInput(tile, firstWord, letter);
+    }
+    searchLetterMatches(sortedAnswers, maxDist, randXY, 'horizontal');
+  }
+}
+
+function searchLetterMatches(sortedAnswers, maxDist, randXY, direction) {
+  var word1 = sortedAnswers[0].text;
+  var rowsColsAmount = document.getElementsByClassName('answer-row').length;
+
+  if (sortedAnswers.length > 1) {
+    var word2 = sortedAnswers[1].text;
+  }
+
+  for (var i = 0; i < word2.length; i++) {
+    var indexOf = word1.indexOf(word2[i]);
+    var spaceLeft = rowsColsAmount - randXY;
+    var matchingWordLen = word2.length;
+
+    if (indexOf > 0 && spaceLeft >= matchingWordLen) {
+
+      // console.log(word1 + ', ' + word2 + ', ' + indexOf);
+
+      //TODO
+      // count if enogh space to place next word
+      //if not enough - iterate through next matching letters
+      // if no matches in current word - skip to next word
+      //if no space to place word legally - place randomly on board
 
       sortedAnswers = sortedAnswers.slice(1);
-      placeWords(sortedAnswers, direction, (maxDist + indexOf), (randXY + i));
+      placeWords(sortedAnswers, direction, indexOf, randXY - i);
+
       return;
 
     }
   }
 }
 
-
-function placeOtherWords(words, direction) {
-  var firstWord = words[0];
-  var otherWords = words.slice(1);
+//   //Math.rand choose one of previous words and place on board crossing with it
 }
 
-function placeOnBoard(wordsArray) {
-  var placed = document.getElementsByClassName('letter-box');
-
-  function findEmptySpace(word, axis, axisNo) {
-
-    var rowAxis = document.querySelectorAll('[data-' + axis + '="' + axisNo + '"]');
-    var filtered = [];
-
-    function findEmptyTile() {
-      var firstEmpty = rowAxis.querySelectorAll('.empty')[0];
-    }
-
-    for (var i = 0; i < rowAxis.length; i++) {
-      if (rowAxis[i].childNodes.length === 0) {
-        filtered.push(rowAxis[i]);
-      } else {
-        filtered = [];
-        //start the loop with next empty element
-      }
-    }
-  }
-
-  //
-  // else {
-  //   //for every letter of current word check if matches any of previous word
-  //   //Math.rand choose one of previous words and place on board crossing with it
-  //
-  //   if (!matches.length) {
-  //     //find random n empty tiles where n === word length
-  //   }
-  // }
-}
-
-//TODO
-//
-// function checkMatchingLetters(answer) {
-//
-//   // let previous;
-//
-//   /*TODO
-//   * check if letters match
-//   * if so - render in opposite direction:
-//     - slice answer: prefix-commonPart-appendix
-//
-//   */
-//
-// };
 //
 // function focusNextLetter() {};
 // function validateAnswer() {};
